d045e03ac290da667f5bfd1c4789c250a44cabda
Navaneeth <32010211+navaneeth-dev@users.noreply.github.com>
2025-05-10 17:49:59 +0530

    fix: argocd bootstrap kustomization ns

--- infrastructure/kubernetes/index.ts
+++ infrastructure/kubernetes/index.ts
@@ -1,18 +1,8 @@
 import * as k8s from "@pulumi/kubernetes";
 
-const argoCDNamespace = new k8s.core.v1.Namespace("argocd-ns", {
-  metadata: {
-    name: "argocd",
-  },
+const argoCD = new k8s.kustomize.v2.Directory("argoCDKustomize", {
+  directory: "./bootstrap",
 });
-
-const argoCD = new k8s.yaml.v2.ConfigFile(
-  "argocd",
-  {
-    file: "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/ha/install.yaml",
-  },
-  { dependsOn: [argoCDNamespace] }
-);
 
 const argoCDRootApp = new k8s.yaml.v2.ConfigFile(
   "argocd-root-app",
