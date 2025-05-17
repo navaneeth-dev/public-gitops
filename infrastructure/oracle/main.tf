f978e3a0fdebb2750f9f037f83310c2c028ea05a
Navaneeth <32010211+navaneeth-dev@users.noreply.github.com>
2025-05-17 22:27:22 +0530

    chore: 1 vcpu default
    Signed-off-by: Navaneeth <32010211+navaneeth-dev@users.noreply.github.com>

--- infrastructure/oracle/main.tf
+++ infrastructure/oracle/main.tf
@@ -5,7 +5,7 @@ variable "instance_shape" {
   default = "VM.Standard.A1.Flex"
 }
 
-variable "instance_ocpus" { default = 2 }
+variable "instance_ocpus" { default = 1 }
 
 variable "instance_shape_config_memory_in_gbs" { default = 8 }
 
